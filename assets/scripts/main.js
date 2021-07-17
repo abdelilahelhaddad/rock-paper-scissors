b09332090c04b9cd44c6235fcf30df872b79a0e9
Abdelilah EL Haddad <elhaddadabdelilah@gmail.com>
2021-07-17 23:49:55 +0100

    :fire: :art: Third Commit Desktop Layout :sparkles: :globe_with_meridians:

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -14,6 +14,12 @@ close_nav.addEventListener('click', () => {
 
 const circles = document.querySelectorAll(".circles");
 const scoreEl = document.querySelector("#score");
+const game = document.querySelector(".game");
+const picked = document.querySelector(".picked");
+const playAgain = document.querySelector(".playAgain");
+const computerSelection = document.querySelector("#computerSelection");
+const userSelection = document.querySelector("#userSelection");
+const winning = document.querySelector("#winning");
 
 const computerChoices = ["paper", "scissors", "rock"];
 
@@ -27,18 +33,32 @@ circles.forEach((circle) => {
   });
 })
 
+playAgain.addEventListener("click", () => {
+  game.style.display = "flex";
+  picked.style.display = "none";
+})
+
 function checkWinner() {
   const computerChoice = computerRandomChoice();
 
+  updateSelection(userSelection, userChoice);
+  updateSelection(computerSelection, computerChoice);
+
   if (userChoice === computerChoice) {
     //draw
+    winning.innerText = "Draw"
   } else if ((userChoice === "paper" && computerChoice === "rock") || (userChoice === "rock" && computerChoice === "scissors") || (userChoice === "scissors" && computerChoice === "paper")) {
     //user won
+    winning.innerText = "You Win"
     updateScore(1);
   } else {
     //user lost
+    winning.innerText = "You Lose"
     updateScore(-1);
   }
+
+  game.style.display = "none";
+  picked.style.display = "flex";
 }
 
 function updateScore(value) {
@@ -49,4 +69,21 @@ function updateScore(value) {
 
 function computerRandomChoice() {
   return computerChoices[Math.floor(Math.random() * computerChoices.length)];
+}
+
+function updateSelection(selectedEL, choice) {
+
+  selectedEL.children[2].classList.remove("paperBorder");
+  selectedEL.children[2].children[0].classList.remove("paper");
+  selectedEL.children[2].classList.remove("scissorsBorder");
+  selectedEL.children[2].children[0].classList.remove("scissors");
+  selectedEL.children[2].classList.remove("rockBorder");
+  selectedEL.children[2].children[0].classList.remove("rock");
+
+  const img = selectedEL.querySelector(".icon-img");
+
+  selectedEL.children[2].classList.add(`${choice}Border`);
+  selectedEL.children[2].children[0].classList.add(choice);
+  img.src = `./assets/images/icon-${choice}.svg`;
+  img.alt = choice;
 }
