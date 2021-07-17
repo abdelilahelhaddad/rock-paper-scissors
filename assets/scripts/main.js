0119e9fffc638e4fd913b7616a4115b55e282a3c
Abdelilah EL Haddad <elhaddadabdelilah@gmail.com>
2021-07-17 21:07:54 +0100

    :fire: :art: Third Commit Desktop Layout :sparkles: :globe_with_meridians:

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -11,3 +11,28 @@ close_nav.addEventListener('click', () => {
   nav_modalo.classList.remove('show');
   open_nav.style.visibility = "visible";
 });
+
+const circles = document.querySelectorAll(".circles");
+const scoreEl = document.querySelector("#score");
+
+const computerChoices = ["paper", "scissors", "rock"];
+
+let gameScore = 0;
+let userChoice = undefined;
+
+circles.forEach((circle) => {
+  circle.addEventListener("click", () => {
+    userChoice = circle.getAttribute("data-choice");
+    console.log(userChoice);
+  });
+})
+
+function updateScore(value) {
+  gameScore += value;
+
+  scoreEl.innerText = gameScore;
+}
+
+function computerRandomChoice() {
+  return computerChoices[Math.floor(Math.random() * computerChoices.length)];
+}
